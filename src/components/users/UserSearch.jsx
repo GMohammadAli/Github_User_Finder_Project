0c9be6f087ce609c3dc5612e3de774d428e995ec
GMohammadAli <gulammohammadali@gmail.com>
2022-08-30 23:30:08 +0530

    Project Complete

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useState, useContext } from 'react'
 import GithubContext from '../../context/github/GithubContext'
 import AlertContext from '../../context/alert/AlertContext'
